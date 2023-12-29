76a711e8f22a27844a840e4fa7aea92d60d0268b
baglayan <baglayan19@itu.edu.tr>
2023-12-29 04:30:04 +0300

    Implement most of the game

--- client.py
+++ client.py
@@ -1,111 +1,191 @@
+# Meriç Bağlayan
+# 150190056
+# Client for BLG 433E Socket Programming assignmnet
+
+# Requires Python 3.10 or higher
+
+# Usage: python3 client.py [<private string> <server address:port>]
+
 from socket import *
 import hashlib
 import threading
-import struct
+import time
+import sys
+import os
 
-def create_socket(serverName, serverPort):
-    clientSocket = socket(AF_INET, SOCK_STREAM)
+in_game: bool = False
+game_start_event: threading.Event = threading.Event()
+
+def create_socket(serverName: str, serverPort: int) -> socket:
+    clientSocket: socket = socket(AF_INET, SOCK_STREAM)
     clientSocket.connect((serverName, serverPort))
     return clientSocket
 
-def receive_async(sock):
-    print('Start receive_async')
+def receive_async(sock: socket) -> None:
+    global in_game, game_start_event
+    payload_size: int
+    payload: bytes
     while True:
-            message_type = sock.recv(1)
+        message_type: bytes = sock.recv(1)
+        if message_type:
             match message_type:
                 case b'\x00':
-                    payload_size = sock.recv(1)
-                    payload = sock.recv(payload_size[0]).decode()
-                    print('[SERVER] ' + payload)
+                    in_game = True
+                    game_start_event.set()
+                    payload_size: int = int.from_bytes(sock.recv(1))
+                    payload: bytes = sock.recv(payload_size)
+                    message: str = payload.decode()
+                    print('[SERVER] ' + message)
                 case b'\x01':
-                    payload_size = sock.recv(1)
-                    payload = sock.recv(payload_size[0])
-                    print('[SERVER] Remaining time: ' + struct.unpack('>H', payload)[0])
+                    payload_size: int = int.from_bytes(sock.recv(1), 'big')
+                    payload = sock.recv(payload_size)
+                    print('[SERVER] Remaining time: ' + payload.decode())
                 case b'\x02':
-                    payload_size = sock.recv(1)
-                    payload = sock.recv(payload_size[0])
-                    print('[SERVER] Game over: ' + struct.unpack('>h', payload)[0])
+                    in_game = False
+                    payload_size: int = int.from_bytes(sock.recv(1), 'big')
+                    payload = sock.recv(payload_size)
+                    print('[SERVER] ' + payload.decode())
                 case _:
                     print('Unknown message type received. Aborting...')
                     break
-        
-def send_async(sock):
-    print('Available commands: start, end, time, <guess>')
-    while True:
-        command = input()
-        match command:
-            case 'start':
-                send_command(sock, 0, None)
-            case 'end':
-                send_command(sock, 1, None)
-            case 'time':
-                send_command(sock, 2, None)
-            case _:
-                send_command(sock, 3, command.encode())
 
-def send_message(sock, message):
+def send_async_in_game(sock: socket) -> None:
+    global in_game, game_start_event
+    while True:
+        while in_game:
+            game_start_event.wait()
+            print('Available commands: end, time, <guess>')
+            print('Available guesses: a number from 0 to 36 (inclusive), \'even\', \'odd\'')
+            command: str = input()
+            
+            match command:
+                case 'end':
+                    send_command(sock, bytearray(b'\x01'), None)
+                case 'time':
+                    send_command(sock, bytearray(b'\x02'), None)
+                case _:
+                    send_command(sock, bytearray(b'\x03'), bytearray(command.encode()))
+            game_start_event.clear()
+            
+def send_async_not_game(sock: socket) -> None:
+    global in_game, game_start_event
+    while True:
+        while not in_game:
+            print('Available commands: start, exit')
+            command: str = input()
+            
+            match command:
+                case 'start':
+                    send_command(sock, bytearray(b'\x00'), None)
+                    in_game = True
+                case 'exit':
+                    send_command(sock, bytearray(b'\x01'), None)
+                    close_socket(sock)
+                    os._exit(1)
+                case _:
+                    print('Unknown command.')
+
+def send_message(sock: socket, message: str) -> None:
     sock.send(message.encode())
     
-def send_command(sock, type, data):
-    command = bytearray()
-    command.append(type & 0xff) # ensure it's a byte
+def send_command(sock: socket, type: bytearray, data: bytearray | None) -> None:
+    command: bytearray = bytearray(type)
     
     if data:
-        payload_size = len(data)
-        command.append(payload_size & 0xff)
-    
-    match type:
-        case 0, 1, 2:
-            pass
-        case 3:
-            command.extend(data.encode())
-            
+        payload_size: bytes = len(data).to_bytes(1, 'big')
+        command.extend(payload_size)
+        command.extend(data)
+
     sock.send(command)
 
-def authenticate(sock, privateString):
-    randomString = sock.recv(32).decode()
+def authenticate(sock: socket, privateString: str) -> None:
+    randomString: str = sock.recv(32).decode()
     if len(randomString) != 32:
         raise RuntimeError('Non-standard random string received. Aborting...')
-    concatString = privateString + randomString
+    concatString: str = privateString + randomString
 
     if len(concatString) != 64:
         raise RuntimeError('Non-standard auth string detected. Aborting...')
 
-    sha1Result = hashlib.sha1(concatString.encode()).hexdigest()
+    sha1Result: str = hashlib.sha1(concatString.encode()).hexdigest()
 
     send_message(sock, sha1Result)
 
-    postShaMessage = sock.recv(1024).decode()
+    postShaMessage: str = sock.recv(1024).decode()
     print('[SERVER] ' + postShaMessage)
 
     if postShaMessage == 'Authentication succesful. Do you wish to proceed?' or postShaMessage == 'Authentication successful. Do you wish to proceed?':
-        send_message(sock, input())
+        print('Y or N?')
+        proper_response: bool = False
+        
+        while not proper_response:
+            proceedResponse: str = input()
+            match proceedResponse.capitalize():
+                case 'Y':
+                    proper_response = True
+                    send_message(sock, 'Y')
+                    return
+                case 'N':
+                    proper_response = True
+                    send_message(sock, 'N')
+                    close_socket(sock)
+                    os._exit(1)
+                case _:
+                    print('Unknown response received. Y or N?')
+    else:
+        print('Unknown message received. Aborting...')
+        close_socket(sock)
+        os._exit(1)
 
-def close_socket(sock):
+def close_socket(sock: socket) -> None:
     sock.close()
 
-def main():
-    privateString = '43d48a355933d4964751cd8c3d1f4ffe'
-    serverName = 'localhost'
-    serverPort = 12000
+privateString: str = 'TSnYnfiJyMQYZqAbfptOPFfsmpWNMNEx'
+serverName: str = '13.48.194.17'
+serverPort: int = 3650
 
-    clientSocket = create_socket(serverName, serverPort)
+def main() -> None:
+    if len(sys.argv) > 1:
+        privateString = sys.argv[1]
+    elif len(sys.argv) > 2:
+        serverName = sys.argv[2].split(':')[0]
+        serverPort = int(sys.argv[2].split(':')[1])
+    else:
+        print('No arguments provided. Using default values for private string and server address.')
+        print('To use custom values, run the program in this format: python3 client.py <private string> <server address:port>')
 
-    startConnectionString = 'Start_Connection'
-    send_message(clientSocket, startConnectionString)
+        privateString: str = '43d48a355933d4964751cd8c3d1f4ffe'
+        serverName: str = 'localhost'
+        serverPort: int = 12000
+        
+        clientSocket: socket
+        print('Attempting to connect to server...')
+        
+        for connection_attempt in range(3):
+            try:
+                clientSocket = create_socket(serverName, serverPort)
+                break
+            except ConnectionRefusedError:
+                if connection_attempt < 2:
+                    print('Connection to server refused. Retrying...')
+                    time.sleep(2)
+                else:
+                    print('Could not connect to the server. Aborting...')
+                    os._exit(1)
 
-    authenticate(clientSocket, privateString)
+        startConnectionString: str = 'Start_Connection'
+        
+        send_message(clientSocket, startConnectionString)
+        authenticate(clientSocket, privateString)
+        
+        receive_thread: threading.Thread = threading.Thread(target=receive_async, args=(clientSocket,))
+        receive_thread.start()
 
-    receive_thread = threading.Thread(target=receive_async, args=(clientSocket,))
-    receive_thread.start()
-
-    send_thread = threading.Thread(target=send_async, args=(clientSocket,))
-    send_thread.start()
-
-    receive_thread.join()
-    send_thread.join()
-
-    close_socket(clientSocket)
+        send_thread_in_game: threading.Thread = threading.Thread(target=send_async_in_game, args=(clientSocket,))
+        send_thread_in_game.start()
+        
+        send_thread_not_game: threading.Thread = threading.Thread(target=send_async_not_game, args=(clientSocket,))
+        send_thread_not_game.start()
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -1,107 +1,290 @@
+# Meriç Bağlayan
+# 150190056
+# Server for BLG 433E Socket Programming assignmnet
+
+# Requires Python 3.10 or higher
+
 from socket import *
 import hashlib
 import secrets
 import string
 import random
-import struct
+import threading
+import time
 
-def create_socket():
+number: int
+remaining_time: int = 30
+in_game = False
+points = 0
+
+def wait_start(sock: socket) -> None:
+    log('Waiting for client to initiate game start')
+    while True:
+        message_type: bytes = sock.recv(1)
+        if message_type:
+            match message_type:
+                case b'\x00':
+                    log('Client initiated game start')
+                    start_game(sock)
+                case b'\x01':
+                    log('Client quit before the game was even started')
+                    break
+                case _:
+                    raise RuntimeError('Unknown packet type specified. Aborting')
+                
+def timer_async(sock: socket) -> None:
+    global remaining_time, in_game, points
+    
+    log('Timer started')
+    
+    while True:
+        if not in_game:
+            log('Timer stopped: not in game')
+            return
+        send_server_message(sock, bytearray(b'\x01'), bytearray(remaining_time))
+        remaining_time -= 3
+        if remaining_time <= 0:
+            points -= 1
+            game_log('1 point deducted from client')
+            game_log(f'Client now has {points} points')
+            send_server_message(sock, bytearray(b'\x02'), bytearray("Your time is up! Be quicker next time.", 'utf-8'))
+            send_server_message(sock, bytearray(b'\x02'), bytearray(f"You have {points} points.", 'utf-8'))
+            in_game = False
+            return
+        time.sleep(3)
+
+def start_game(sock: socket) -> None:
+    global in_game, remaining_time
+    in_game = True
+    remaining_time = 30
+    
+    log('Game started.')
+
+    global number
+    number = random.randint(0, 36)
+    log('Number is ' + str(number))
+    
+    update_game(sock)
+    
+    
+def update_game(sock: socket) -> None:
+    global number, points, in_game
+    while True:
+        if not in_game:
+            return
+        while in_game:
+            send_server_message(sock, bytearray(b'\x00'), bytearray("What is your guess? Number, even, odd?", 'utf-8'))
+            message_type: bytes = sock.recv(1)
+            
+            match message_type:
+                case b'\x01':
+                    in_game = False
+                case b'\x02':
+                    log('Client requested time')
+                    send_server_message(sock, bytearray(b'\x01'), bytearray(remaining_time))
+                case b'\x03':
+                    payload_size: int = int.from_bytes(sock.recv(1))
+                    
+                    guess_payload: bytes  = sock.recv(payload_size)
+                    guess_str = guess_payload.decode()
+                    guess = int | str
+                    
+                    is_guess_number: bool = False
+                    
+                    if guess_str.isnumeric():
+                        guess = int(guess_str)
+                        is_guess_number = True
+                    elif guess_str.lower() == 'even' or guess_str.lower() == 'odd':
+                        guess = guess_str.lower()
+                        
+                    match is_guess_number:
+                        case True:
+                            game_log('Number received.')
+                            handle_number_guess(sock, guess)
+                        case False:
+                            handle_word_guess(sock, guess)
+                            
+def handle_number_guess(sock: socket, guess: int | str | type[int] | type[str]) -> None:
+    global points, in_game
+    if guess == number:
+        game_log('Correct guess')
+        points += 35
+        game_log('35 points added to client')
+        game_log(f'Client now has {points} points')
+        send_server_message(sock, bytearray(b'\x02'), bytearray("You won! Congratulations.", 'utf-8'))
+        send_server_message(sock, bytearray(b'\x02'), bytearray(f"You have {points} points.", 'utf-8'))
+        in_game = False
+    else:
+        game_log('Wrong guess')
+        points -= 1
+        game_log('1 point deducted from client')
+        game_log(f'Client now has {points} points')
+        send_server_message(sock, bytearray(b'\x02'), bytearray("You lost! Better luck next time.", 'utf-8'))
+        send_server_message(sock, bytearray(b'\x02'), bytearray(f"You have {points} points.", 'utf-8'))
+        in_game = False
+
+def handle_word_guess(sock: socket, guess: str | int | type[int] | type[str]) -> None:
+    global points, in_game
+    if guess == "even":
+        game_log('Even received.')
+        if number % 2 == 0:
+            game_log('Correct guess')
+            points += 1
+            game_log('1 point added to client')
+            game_log(f'Client now has {points} points')
+            send_server_message(sock, bytearray(b'\x02'), bytearray("You won! Congratulations.", 'utf-8'))
+            send_server_message(sock, bytearray(b'\x02'), bytearray(f"You have {points} points.", 'utf-8'))
+            in_game = False
+        else:
+            game_log('Wrong guess')
+            points -= 1
+            game_log('1 point deducted from client')
+            game_log(f'Client now has {points} points')
+            send_server_message(sock, bytearray(b'\x02'), bytearray("You lost! Better luck next time.", 'utf-8'))
+            send_server_message(sock, bytearray(b'\x02'), bytearray(f"You have {points} points.", 'utf-8'))
+            in_game = False
+    elif guess == "odd":
+        game_log('Odd received.')
+        if number % 2 == 0:
+            game_log('Wrong guess')
+            points -= 1
+            game_log('1 point deducted from client')
+            game_log(f'Client now has {points} points')
+            send_server_message(sock, bytearray(b'\x02'), bytearray("You lost! Better luck next time.", 'utf-8'))
+            send_server_message(sock, bytearray(b'\x02'), bytearray(f"You have {points} points.", 'utf-8'))
+            in_game = False
+        else:
+            game_log('Correct guess')
+            points += 1
+            game_log('1 point added to client')
+            game_log(f'Client now has {points} points')
+            send_server_message(sock, bytearray(b'\x02'), bytearray("You won! Congratulations.", 'utf-8'))
+            send_server_message(sock, bytearray(b'\x02'), bytearray(f"You have {points} points.", 'utf-8'))
+            in_game = False            
+        
+def log(message: str) -> None:
+    print('[LOG] ' + message)
+    
+def game_log(message: str) -> None:
+    print('[GAME] ' + message)
+    
+def auth_log(message: str) -> None:
+    print('[AUTH] ' + message)
+
+def create_socket() -> socket:
     return socket(AF_INET, SOCK_STREAM)
 
-def bind_socket(sock, port):
-    sock.bind(('', port))
+def bind_socket(sock: socket, port: int) -> None:
+    try:
+        sock.bind(('', port))
+    except OSError:
+        log('Port is already in use. Aborting')
+        exit()
 
-def start_listening(sock):
+def start_listening(sock: socket) -> None:
     sock.listen(1)
 
-def accept_connection(sock):
+def accept_connection(sock: socket) -> tuple[socket, tuple[str, int]]:
     return sock.accept()
 
-def send_packet(sock, message):
-    sock.send(message.encode())
+def send_packet(sock: socket, message: bytearray) -> None:
+    try:
+        sock.send(message)
+    except OSError as e:
+        log(f"Error sending data: {e}")
     
-def send_server_message(sock, type, data):
-    message = bytearray()
-    message.append(type & 0xff) # ensure it's a byte
+def send_bytearray(sock: socket, message: bytearray) -> None:
+    try:
+        sock.send(message)
+    except OSError as e:
+        log(f"Error sending data: {e}")
     
-    payload_size = len(data)
-    message.append(payload_size & 0xff)
+def send_server_message(sock: socket, type: bytearray, data: bytearray) -> None:
+    message: bytearray = bytearray()
+    message.extend(type)
     
-    match type:
-        case 0:
-            message.extend(data.encode())
-        case 1:
-            message.extend(struct.pack('>H', data))
-        case 2:
-            message.extend(struct.pack('>h', data))
+    payload_size: int = len(data)
+    if 0 > payload_size or payload_size > 255:
+        raise RuntimeError('Payload size is beyond limits. Aborting')
+    payload_size_bytes: bytes = bytes([payload_size])
+    message.extend(payload_size_bytes)
+    
+    match bytes(type):
+        case b'\x00':
+            message.extend(data)
+        case b'\x01':
+            message.extend(data)
+        case b'\x02':
+            message.extend(data)
         case _:
-            raise RuntimeError('Unknown packet type specified. Aborting...')
+            raise RuntimeError('Unknown packet type specified. Aborting')
             
-    send_packet(sock, message)
+    send_bytearray(sock, message)
 
-def receive_packet(sock, buffer_size):
+def receive_packet(sock: socket, buffer_size: int) -> str:
     return sock.recv(buffer_size).decode()
 
-def close_connection(sock):
+def close_connection(sock: socket) -> None:
     sock.close()
 
-def start_game(sock):
-    print('Game started.')
-
-    number = random.randint(0, 36)
-    print('Number is ' + str(number))
-    send_server_message(sock, 0, "What is your guess? Number, even, odd?")
-
-def update_game(sock):
-    print('Update')
-
-def authenticate(privateString, connectionSocket):
-    randomString = ''.join(secrets.choice(string.ascii_uppercase + string.digits) for _ in range(32))
-    send_packet(connectionSocket, randomString)
+def authenticate(privateString: str, connectionSocket: socket) -> bool:
+    randomString: str = ''.join(secrets.choice(string.ascii_uppercase + string.digits) for _ in range(32))
+    send_packet(connectionSocket, bytearray(randomString, 'utf-8'))
     
-    concatString = privateString + randomString
-    calculatedHash = hashlib.sha1(concatString.encode()).hexdigest()
+    concatString: str = privateString + randomString
+    calculatedHash: str = hashlib.sha1(concatString.encode()).hexdigest()
     
-    receivedHash = receive_packet(connectionSocket, 40)
+    receivedHash: str = receive_packet(connectionSocket, 40)
     
     if len(receivedHash) != 40:
-        raise RuntimeError('Non-standard auth string received. Aborting...')
+        raise RuntimeError('Non-standard auth string received. Aborting')
     
     if calculatedHash == receivedHash:
-        print('Authentication successful!')
-        successMessage = 'Authentication succesful. Do you wish to proceed?'
-        send_packet(connectionSocket, successMessage)
-        proceed = receive_packet(connectionSocket, 1)
+        auth_log('Authentication successful!')
+        successMessage: str = 'Authentication succesful. Do you wish to proceed?'
+        send_packet(connectionSocket, bytearray(successMessage, 'utf-8'))
         
-        if proceed.capitalize() == 'Y':
-            start_game(connectionSocket)
-        elif proceed.capitalize() == 'N':
-            close_connection(connectionSocket)
-            return False
     else:
-        print('Authentication failed.')
-        unauthorizedMessage = 'Unauthorized access.'
-        send_packet(connectionSocket, unauthorizedMessage)
+        auth_log('Authentication failed.')
+        unauthorizedMessage: str = 'Unauthorized access.'
+        send_packet(connectionSocket, bytearray(unauthorizedMessage, 'utf-8'))
     return True
 
-def main():
-    privateString = '43d48a355933d4964751cd8c3d1f4ffe'
+def main() -> None:
+    privateString: str = '43d48a355933d4964751cd8c3d1f4ffe'
 
-    serverPort = 12000
-    serverSocket = create_socket()
+    serverPort: int = 12000
+    serverSocket: socket = create_socket()
     bind_socket(serverSocket, serverPort)
     start_listening(serverSocket)
 
-    print('Awaiting Start_Connection message...')
+    log('Awaiting Start_Connection message')
 
     while True:
         connectionSocket, addr = accept_connection(serverSocket)
-        message = receive_packet(connectionSocket, 1024)
+        print(addr)
+        message: str = receive_packet(connectionSocket, 1024)
         if message == 'Start_Connection':
-            print('Beginning auth process...')
+            auth_log('Beginning auth process')
             if not authenticate(privateString, connectionSocket):
                 break
-        close_connection(connectionSocket)
+            
+            proceed: str = receive_packet(connectionSocket, 1)
+        
+            if proceed.capitalize() == 'Y':
+                log('Game accepted by client')
+                
+                game_thread = threading.Thread(target=wait_start, args=(connectionSocket,))
+                game_thread.start()
+                
+                timer_thread = threading.Thread(target=timer_async, args=(connectionSocket,))
+                timer_thread.start()
+                
+            elif proceed.capitalize() == 'N':
+                log('Game rejected by client')
+                # close_connection(connectionSocket)
+                continue
+        # close_connection(connectionSocket)
 
 if __name__ == "__main__":
     main()
